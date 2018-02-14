0f07268cfd6c6f62f9d7c65286ad26d262adf861
KINGQB7 <cshoon421@gmail.com>
2018-02-14 23:09:03 +0900

    commit

--- Algospot/2_xhaeneung.py
+++ Algospot/2_xhaeneung.py
@@ -5,9 +5,12 @@ for i in range(n_iter):
     str_split = str_input.split()
     A = str_split[0]
     operator = str_split[1]
+    if operator not in ["+", "*", "-"]:
+        print("No")
+        continue
     B = str_split[2]
     C = str_split[4]
-    str_number = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, 'ten': 10}
+    str_number = dict(zero=0, one=1, two=2, three=3, four=4, five=5, six=6, seven=7, eight=8, nine=9, ten=10)
     a = str(str_number.get(A))
     b = str(str_number.get(B))
     formula_c = a + operator + b
@@ -18,17 +21,17 @@ for i in range(n_iter):
         continue
     else:
         str_c = list(str_number.keys())[c]
-        if (len(str_c) != len(C)) or (set(str_c) != set(C) ):
+        if (len(str_c) != len(C)) or (set(str_c) != set(C)):
             print("No")
             continue
         else:
             temp = list(set(C))
             for ss in range(len(set(C))):
-                if (str_c.count(list(set(C))[ss]))==(C.count(list(set(C))[ss])):
+                if (str_c.count(list(set(C))[ss])) == (C.count(list(set(C))[ss])):
                     temp[ss] = 1
                 else:
                     temp[ss] = 0
-            if sum(temp)==len(set(C)):
+            if len(set(C)) == sum(temp):
                 print("Yes")
             else:
                 print("No")
